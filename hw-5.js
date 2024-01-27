8bd37ac37b2ea8131f1d37b0b69f203ada753b36
MSI <eao173a@gmail.com>
2024-01-27 21:19:24 +0800

    End hw-5

--- hw-5.js
+++ hw-5.js
@@ -6,7 +6,7 @@ const minimumNumber = (a, b) => {
 
 //h2
 const evenOrOdd = (a) => {
-    return (a % 2 == 0) ? 'Число четное': 'Число нечетное';
+    return (a % 2 == 0) ? 'Число четное' : 'Число нечетное';
 }
 
 //h3
@@ -32,7 +32,51 @@ const ageUser = () => {
 
 //h5
 const numberOrNan = (a, b) => {
-   return (!Number.isNaN(a) && !Number.isNaN(b)) ? a * b: 'Одно или оба значения не являются числом';
+    return (!Number.isNaN(a) && !Number.isNaN(b)) ? a * b : 'Одно или оба значения не являются числом';
 }
 
-//
+//h6
+const cube = () => {
+    let num = prompt('Введите число, которое нужно возвести в квадрат.');
+    return (!isNaN(num)) ? `${num} в кубе равняется ${num *= num}` :
+        'Переданный параметр не является числом';
+}
+
+//h7
+const circle1 = {
+    radius: 3,
+    getArea() {
+        return 3.14 * (this.radius *= this.radius);
+    },
+    getPerimeter() {
+        return 2 * 3.14 * this.radius;
+    }
+}
+
+const circle2 = {
+    radius: 3,
+    getArea() {
+        return 3.14 * (this.radius *= this.radius);
+    },
+    getPerimeter() {
+        return 2 * 3.14 * this.radius;
+    }
+}
+
+//h8
+
+function seasonsGame() {
+    let monthNumber = +(prompt('Ведите число от 1 до 12'));
+
+    if (monthNumber === 1 || monthNumber === 2 || monthNumber === 12) {
+        alert('Зима');
+    } else if (monthNumber === 3 || monthNumber === 4 || monthNumber === 5) {
+        alert('Весна');
+    } else if (monthNumber === 6 || monthNumber === 7 || monthNumber === 8) {
+        alert('Лето');
+    } else if (monthNumber === 9 || monthNumber === 10 || monthNumber === 11) {
+        alert('Осень');
+    } else {
+        alert('Поддерживается только числа от 1 до 12!');
+    }
+}
